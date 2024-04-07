81826bb8390e84b8958db203d5728258d1d6f2a1
mlcrb <thesushka72@gmail.com>
2024-04-07 13:36:00 +0300

    Fix tooltip

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $('.sprite').hover(function () {
   var name = this.getAttribute("name").toString();
-  $('body').append('<span id="name">' + name + '</span>');
-  $('#name').css({
+  $('#tooltip').css({
     'position': 'absolute',
     'z-index': '4',
     'background-color': 'rgba(16,0,17,0.9)',
@@ -10,18 +9,19 @@ $('.sprite').hover(function () {
     'border': '2px solid #210565',
     'line-height': '1.5'
   });
+  var content;
   switch (name) {
     case 'Нити лозы':
-      $('#name').css('color', '#00aa00');
+      content = `<span style="color: #00aa00">${name}</span>`
       break;
     case 'Устройство Возраждатель':
-      $('#name').html('<span style="color: #ffff55">Устройство</span><br><span style="color: #fff">Возраждатель</span>');
+      content = '<span style="color: #ffff55">Устройство</span><br><span style="color: #fff">Возраждатель</span>';
       break;
     case 'Смесь для выпечки пирога':
-      $('#name').css('color', '#55ffff');
+      content = `<span style="color: #55ffff">${name}</span>`;
       break;
     case 'Железийный топор Эффективность II':
-      $('#name').html('<span style="color: #fff">Железийный топор</span><br><span style="color: #aaa">Эффективность II</span>');
+      content = '<span style="color: #fff">Железийный топор</span><br><span style="color: #aaa">Эффективность II</span>';
       break;
     case 'Смесь для выпечки печенья':
     case 'Заготовка под броню':
@@ -30,29 +30,32 @@ $('.sprite').hover(function () {
     case 'Средний энергоосколок':
     case 'Свекольный набор':
     case 'Грибной набор':
-      $('#name').css('color', '#55ff55');
+      content = `<span style="color: #55ff55">${name}</span>`
       break;
     case 'Деконструктор':
-      $('#name').css('color', '#ffff55');
+      content = `<span style="color: #ffff55">${name}</span>`
       break;
     case 'Содалит':
     case 'Содалитовый кристалл':
     case 'Капля энергии':
-      $('#name').css('color', '#5555ff');
-      break;
-    default:
-      $('#name').css('color', '#fff');
+      content = `<span style="color: #5555ff">${name}</span>`
       break;
     case 'Ножницы Стальные ножницы Пальцы не совать':
-      $('#name').html('<span style="color: #fff">Ножницы</span><br><span style="color: #aaa">Стальные ножницы</span><br><span style="color: #aaa">Пальцы не совать</span>');
+      content = '<span style="color: #fff">Ножницы</span><br><span style="color: #aaa">Стальные ножницы</span><br><span style="color: #aaa">Пальцы не совать</span>';
+      break;
+
+    default:
+      content = name;
       break;
   }
+  $('#tooltip').html(content);
+  $('#tooltip').show();
 }, function () {
-  $('#name').remove();
+  $('#tooltip').hide();
 });
 
 $(document).mousemove(function (e) {
-  $('#name').css({
+  $('#tooltip').css({
     left: e.pageX + 15,
     top: e.pageY - 15
   });
@@ -71,19 +74,10 @@ function copyToClipboard(elem) {
   }
 }
 
-$('.animated').hover(function () {
-  $(this).addClass('animated-pause');
-}, function () {
-  $(this).removeClass('animated-pause');
-});
-
 $(document).ready(
   function (e) {
     setInterval(function () {
       $("body").find('.animated').each(function () {
-        if ($(this).hasClass('animated-paused')) {
-          return;
-        }
         var $nextFrame = advanceFrame(this, '.animated');
         if ($nextFrame.hasClass('animated-subframe')) {
           advanceFrame($nextFrame[0], '.animated-subframe');
